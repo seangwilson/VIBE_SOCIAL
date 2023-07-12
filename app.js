76fcdaeef059902ca1ff2ebeaf5d52fe9e01a570
Sean Wilson <seangwilson@hotmail.com>
2023-07-12 08:40:14 -0300

    Registration and Login completed

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const middleware = require('./middleware')
 const path = require('path')
 const bodyParser = require("body-parser")
 const mongoose = require("./database");
+const session = require("express-session");
 
 const server = app.listen(port, () => console.log("Server listening on port " + port));
 
@@ -13,6 +14,12 @@ app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(session({
+    secret: "shadow",
+    resave: true,
+    saveUninitialized: false
+}))
 
 // Routes
 const loginRoute = require('./routes/loginRoutes');
@@ -24,7 +31,8 @@ app.use("/register", registerRoute);
 app.get("/", middleware.requireLogin, (req, res, next) => {
 
     var payload = {
-        pageTitle: "Home"
+        pageTitle: "Home",
+        userLoggedIn: req.session.user
     }
 
     res.status(200).render("home", payload);
--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -2,18 +2,20 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const bodyParser = require("body-parser")
+const User = require('../schemas/UserSchema');
+const bcrypt = require('bcrypt');
 
 app.set("view engine", "pug");
 app.set("views", "views");
 
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-router.get("/", (req, res, next) => {  
+router.get("/", (req, res, next) => {
 
     res.status(200).render("register");
 })
 
-router.post("/", (req, res, next) => {  
+router.post("/", async (req, res, next) => {
 
     var firstName = req.body.firstName.trim();
     var lastName = req.body.lastName.trim();
@@ -24,13 +26,50 @@ router.post("/", (req, res, next) => {
     var payload = req.body;
 
     if(firstName && lastName && username && email && password) {
+        var user = await User.findOne({
+            $or: [
+                { username: username },
+                { email: email }
+            ]
+        })
+        .catch((error) => {
+            console.log(error);
+            payload.errorMessage = "Something went wrong.";
+            res.status(200).render("register", payload);
+        });
+
+        if(user == null) {
+            // No user found
+
+            var data = req.body;
+
+            data.password = await bcrypt.hash(password, 10)
+
+            User.create(data)
+            .then((user) => {
+                req.session.user = user;
+                return res.redirect("/");
+            })
+        }
+        else {
+            // User found
+
+            if (email == user.email) {
+                payload.errorMessage = "Email already in use.";
+            }
+            else {
+                payload.errorMessage = "Username already in use.";
+            }
+            res.status(200).render("register", payload);
+        }
+        
+        
 
     }
     else {
-        payload.errorMessage = "Make sure each field has a valid value"
+        payload.errorMessage = "Make sure each field has a valid value.";
         res.status(200).render("register", payload);
     }
-    
 })
 
 module.exports = router;
